c22ec962d061972be989e65c9f240d9b1d62cfcc
Paul <pfultz2@yahoo.com>
2017-08-22 11:49:07 -0500

    Set correct requirements for sphinx to fix travis errors

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     install_requires=[
         'commonmark>=0.7.3',
         'docutils>=0.11',
-        'sphinx>=1.3.1',
+        'sphinx>=1.5,<1.6',
     ],
     entry_points={'console_scripts': [
         'cm2html = recommonmark.scripts:cm2html',
