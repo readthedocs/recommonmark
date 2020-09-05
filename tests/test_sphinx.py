a6377324042ca9098a1c7dd0952f8e1e4fdd59ad
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2020-09-06 02:29:29 +0900

    Fix CustomExtensionTests.test_integration failure with docutils 0.16
    All automated tests on Travis for PRs from January has failed because
    docutils 0.16 (2020-01-12) contains a following commit.
    
        HTML fixes and updates.
    
        Remove dead code.
        Remove unused class value "first" for topic-title.
        Fix/update CSS stylesheets for html5 writer.
    
    https://repo.or.cz/docutils.git/commit/e094ffdca98f49dab492ec4f8de5d2be7ec8f23d
    
    This commit is a workaround to that change.

--- tests/test_sphinx.py
+++ tests/test_sphinx.py
@@ -5,6 +5,7 @@ import shutil
 import unittest
 from contextlib import contextmanager
 
+import docutils
 from sphinx.application import Sphinx
 
 
@@ -217,12 +218,16 @@ class CustomExtensionTests(SphinxIntegrationTests):
         self.assertIn('<th class="head">data</th>', output)
         self.assertIn('</table>', output)
 
+        if docutils.__version_info__ >= (0, 16):
+            additional_class = ''
+        else:
+            additional_class = ' first'
         self.assertIn(
             ('<div class="contents topic" id="contents">\n'
-             '<p class="topic-title first">Contents</p>\n'
+             '<p class="topic-title{}">Contents</p>\n'
              '<ul class="simple">\n'
              '<li><a class="reference internal" href="#header" id="id1">Header</a><ul>\n'
              '<li><a class="reference internal" href="#header-2" id="id2">Header 2</a></li>\n'
-             '</ul>\n</li>\n</ul>'),
+             '</ul>\n</li>\n</ul>'.format(additional_class)),
             output
             )
